08ead96ea2601e4063ada26cceb5eae21a68742d
Jessica Brush <jessicadawnbrush@gmail.com>
2018-07-01 20:56:03 -0700

    configure passport

--- mern-secure/app.js
+++ mern-secure/app.js
@@ -7,6 +7,12 @@ var bodyParser = require('body-parser');
 var book = require('./routes/book');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/mern-secure', { promiseLibrary: require('bluebird') })
+  .then(() => console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
--- mern-secure/routes/book.js
+++ mern-secure/routes/book.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Book = require('../models/Book.js');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Express REST API');
+/* GET ALL BOOKS */
+router.get('/', function (req, res, next) {
+  Book.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+/* SAVE BOOK */
+router.post('/', function (req, res, next) {
+  Book.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
 
 module.exports = router;
