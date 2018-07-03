9bd16e417def97483faee82c6350d5c5a0af4f10
Hannah Lim <hannah15198@gmail.com>
2018-07-02 23:29:36 -0700

    cannot open on yarn. Added User in model, Jumbotron in components>Results and pages>Results

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Navbar = () => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-       
+       <a className="navbar-brand" href="/">
+            Footprint Calculator
+       </a>
     </nav>
 );
 
