e42b8686f8d598e18167e47e9e86464f064395e4
Jessica Brush <jessicadawnbrush@gmail.com>
2018-07-10 14:50:27 -0700

    logo in nav and login styling

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,9 +3,11 @@ import "./Navbar.css";
 
 const Navbar = () => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary nav--height">
-       <a className="navbar-brand" href="/">
+       <span><a className="navbar-brand" href="/">
             Footprint Calculator
        </a>
+        <img src="favicon.ico" width="30" height="30" class="d-inline-block align-top" id="logoimg" alt="logo"></ img>
+        </span>
     </nav>
 );
 
--- client/src/components/Video/Video.js
+++ client/src/components/Video/Video.js
@@ -4,16 +4,11 @@ import vid from "./assets/videofarm.webm";
 import "./Video.css";
         
 class Video extends React.Component {
-        //     constructor(props) {
-        // super(props);
-        // this.state = {
-        // };
-    // }
 
     render() {
         return (
             <div>
-                some text<video className="video-container video-container-overlay" autoPlay="true" id="myVideo" loop="true" muted={this.props.muted}>
+                <video className="video-container video-container-overlay" autoPlay="true" id="myVideo" loop="true" muted={this.props.muted}>
                     <source src={vid} type="video/mp4" />
                 </video>
             </div>
