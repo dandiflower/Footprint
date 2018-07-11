44c6356ae946ecacba4af0c874c7d274751a33fe
Hannah Lim <hannah15198@gmail.com>
2018-07-10 17:26:53 -0700

    Pulling new file

--- client/src/components/Slidebar/Slidebar.js
+++ client/src/components/Slidebar/Slidebar.js
@@ -1,51 +1,41 @@
-import React, { Component } from 'react'
-import ReactDom from 'react-dom';
-
-// import "./Slidebar.css";
-
-// Using an ES6 transpiler like Babel
+import React from 'react'
 import Slider from 'react-rangeslider'
-
-// To include the default styles
 import 'react-rangeslider/lib/index.css'
 
 
-// function log(value) {
-//     console.log(value);
-// }
-
-// function percentFormatter(v) {
-//     return `${v} %`;
-// }
-
-// const SliderWithTootTip = createSliderWithTooltip(Slider);
-
 class CustomizedSlider extends React.Component {
-    constructor(props, context) {
-        super(props, context)
-        this.state = {
-          sliderValue: 0
-        }
-      }
-    handleOnChange = (value) => {
-        this.setState({
-          sliderValue: value
-        })
-        return this.props.sliderValues(this.state.sliderValue)
-      }
-     
-      render() {
-        let { sliderValue } = this.state
-        return (
-          <Slider
-            id={this.props.id}
-            value={sliderValue}
-            orientation="horizontal"
-            onChange={this.handleOnChange}
-          />
-        )
+  constructor(props, context) {
+      super(props, context)
+      this.state = {
+        sliderValue: 0
       }
     }
+  handleOnChange = (value) => {
+      this.setState({
+        sliderValue: value
+      })
+      console.log("before change", value)
+      // return this.props.sliderValues(this.state.sliderValue)
+    }
+    // sliderInputHandler =(value) => {
+    //   console.log("after change", value)
+    //   this.setState({
+    //     sliderValue: value
+    //   })
+    // }
+    render() {
+      let { sliderValue } = this.state
+      return (
+        <Slider
+          id={this.props.id}
+          value={sliderValue}
+          orientation="horizontal"
+          onChange={this.handleOnChange}
+          onAfterChange={this.sliderInputHandler}
+        />
+      )
+    }
+  }
 
 export default CustomizedSlider;
 
