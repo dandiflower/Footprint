2de8e4eb7197dbc8ef9cda61b6139ee85c15d2a5
Hannah Lim <hannah15198@gmail.com>
2018-07-12 16:54:26 -0700

    added readme content

--- README.md
+++ README.md
@@ -1,10 +1,97 @@
-# Securing MERN Stack Web Application using Passport
+# Footprint
 
-This source code is part of [Securing MERN Stack Web Application using Passport](https://www.djamware.com/post/5a90c37980aca7059c14297a/securing-mern-stack-web-application-using-passport) tutorial.
+Footprint is a simple survey you can take to see your effect on the environment compared to other users.
+
+
+
+## Getting Started
+
+Open up the [project link](https://infinite-badlands-27209.herokuapp.com/login) and you should be able to registor or login.
+
+![Homepage](assets/LoginPage.PNG)
+
+
+After you are logged in, you must answer all the questions provided in the survey.
+
+![Quiz](assets/QuizPage.PNG)
+
+
+After submitting your survey, you will get your results compared to other users who have taken the same survey.
+
+![Results](assets/Results.PNG)
+
+
+
+## Running Footprint Locally
 
 To run this source code:
 
 * Clone this repo
-* Run MongoDB server
-* Run `npm npm run-script build`
-* Run `npm start`
+* Run MongoDB server. We run `mongod` in the terminal and use Robo 3T to see our database.
+* Run `npm start` or `yarn start`
+
+
+
+## Built With
+
+* HTML5
+
+* CSS
+
+* [Javascript](https://www.javascript.com/)
+
+* [BootstrapCDN v3.3.7](http://getbootstrap.com)
+
+* [MongoDB](http://mongodb.github.io/node-mongodb-native/3.1/) - Database to store and retreive user answers
+
+* [Express js](https://expressjs.com/) - Node application library
+
+* [React.js](https://reactjs.org/) - Component based JavaScript library
+
+* [Node](https://nodejs.org/en/) - Run JavaScript's backend loacally 
+
+* [Mongoose](http://mongoosejs.com/) - Modeling for MongoDB 
+
+* [Heroku](https://www.heroku.com/) - Deployed on Heroku
+
+* [Passport](http://www.passportjs.org/docs/authenticate/) - login and register authentication
+
+* [Chart.js](https://www.chartjs.org/) - creating graphs on the results page
+
+
+
+## Future Features
+
+In the future, we hope to incorporate the following features:
+
+* Carbon Footprint APIs to give users a number they can improve on
+
+* Stripe or Paypal for donations to non-profits focusing on the environment 
+
+* D3 or Chartio to display data visualizations and infographs of user results
+
+* API or calculate ways users can alleviate high carbon footprint
+
+* Allow social media sharing of user results
+
+
+
+## Inspiration
+
+* [Footprint Calculator](http://www.footprintcalculator.org/)
+
+* [Berkley Carbon Footprint Calculator](http://coolclimate.berkeley.edu/carboncalculator)
+
+
+
+## Authors
+
+* **Jessica Brush**  - [dandiflower](https://github.com/dandiflower)
+
+* **Hannah Lim** - [hannahlim213](https://github.com/hannahlim213)
+
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -13,94 +13,104 @@ class Results extends React.Component {
         }
     }
 
-getChartData() {
-    this.setState({
-        chartData: {
-            labels: ['You', 'USA Average'],
-            datasets: [{
-                label: 'Number of Days',
-                data:[
-                    3,
-                    5
-                ],
-                backgroundColor: ['rgba(255,99,132,0.2)', 'rgba(255,99,132,0.2)']
-            }]
-        }
-    });
-}
-
-
-logout() {
-    localStorage.removeItem('jwtToken');
-    window.location.reload();
-}
-
-componentWillMount(){
-    this.getChartData()
-    console.log(this.state.userId)
-    const getUser = {
-        userId: this.state.userId
+    allData = () => {
+        HELPERS.getResults()
+            .then(res => 
+            console.log(res.data)
+            )
     }
-    HELPERS.getResults(this.state.userId)
-    .then(dbResults=>{
-        console.log("dbResults", dbResults)
-        this.setState({
-            firstName: dbResults.data.firstName,
-            q1: dbResults.data.q1,
-        })
-    })
-}
+
+    getChartData = () => {
+        // HELPERS.getResults()
+        //     .then(res =>
+                this.setState({
+                    chartData: {
+                        labels: ['You', 'USA Average'],
+                        datasets: [{
+                            label: 'Number of Days per Week',
+                            //data: [res.data.q1, average]
+                            data: [3, 5],
+                            backgroundColor: ['rgba(255,99,132,0.2)', 'rgba(255,99,132,0.2)']
+                        }]
+                    }
+                })
+            // )
+
+    }
+
+
+    logout() {
+        localStorage.removeItem('jwtToken');
+        window.location.reload();
+    }
+
+    componentWillMount() {
+        // this.allData()
+        this.getChartData()
+        console.log(this.state.userId)
+        const getUser = {
+            userId: this.state.userId
+        }
+        HELPERS.getResults(this.state.userId)
+            .then(dbResults => {
+                console.log("dbResults", dbResults)
+                this.setState({
+                    firstName: dbResults.data.firstName,
+                    q1: dbResults.data.q1,
+                })
+            })
+    }
 
 
 
 
-render(){
-    return(
-        <div>
-            <Navbar />
-            <div id="results--card--center">
-                <div className="container">
-                    <div className="card" >
-                        <div className="card-header">
-                            <h1>See your results {this.state.firstName}!</h1>
-                        </div>
-                        <ul className="list-group list-group-flush">
-                            <li id="listResults" className="list-group-item text-center"><h2>See in the graph below how your footprint compares to the rest of people!</h2></li>
-                            <li className="text-center">  </li>
-                        </ul>
-                    
-                        {/* first question */}
-                        <Chart 
-                        chartData={this.state.chartData}
-                        location='How much meat you eat compared to the average US citizen'
-                        legendPosition="bottom"
-                        height={400}
-                        max={10}
-                        />   
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <div id="results--card--center">
+                    <div className="container">
+                        <div className="card" >
+                            <div className="card-header">
+                                <h1>See your results {this.state.firstName}!</h1>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                <li id="listResults" className="list-group-item text-center"><h2>See in the graph below how your footprint compares to the rest of people!</h2></li>
 
-                        {/* second question  */}
-                        <Chart 
-                        
-                        />
-                      
+                            </ul>
 
-                        <p>
-                            {localStorage.getItem('jwtToken') &&
-                                <button
-                                    className="btn btn-primary"
-                                    onClick={this.logout}>
-                                    Logout
+                            {/* first question */}
+                            <Chart
+                                chartData={this.state.chartData}
+                                location='How much meat you eat compared to the average US citizen'
+                                legendPosition="bottom"
+                                height={400}
+                                max={10}
+                            />
+
+                            {/* second question  */}
+                            <Chart
+
+                            />
+
+
+                            <p>
+                                {localStorage.getItem('jwtToken') &&
+                                    <button
+                                        className="btn btn-primary"
+                                        onClick={this.logout}>
+                                        Logout
                                 </button>
-                            }
-                        </p>
+                                }
+                            </p>
 
+                        </div>
                     </div>
                 </div>
+
             </div>
-           
-        </div>
-    );
-}
+        );
+    }
 
 
 }
