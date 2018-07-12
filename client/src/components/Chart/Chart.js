e702fbd4751a8f4081d9e07f727e0accde12b206
Hannah Lim <hannah15198@gmail.com>
2018-07-11 17:19:22 -0700

    some space stylin on form and getting started on charts

--- client/src/components/Chart/Chart.js
+++ client/src/components/Chart/Chart.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
+import LineChart from 'react-chartjs';
 
 class Chart extends Component {
+    
     render(){
+        lineChart = () => {
+            const config = {
+                type: 'line',
+                datasets: [{
+                    label:['You'],
+                    backgroundColor: red,
+                    data: [
+                        
+                    ]
+                }]
+            }
+        }
         return(
             <div className="chart">
-                CHART COMPONENT
+                
             </div>
         );
     }
