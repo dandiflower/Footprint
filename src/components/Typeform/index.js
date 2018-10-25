55828ecc611036264f5ccc1bd10fbbea762861dc
Hannah Lim <hannah15198@gmail.com>
2018-10-25 16:46:41 -0700

    working popup button to typeform survey

--- src/components/Typeform/index.js
+++ src/components/Typeform/index.js
@@ -3,23 +3,6 @@ import * as typeformEmbed from '@typeform/embed'
 
 class Typeform extends Component{
     
-    componentDidMount() {
-        const reference = document.querySelector('.target-dom-node')
-        typeformEmbed.makePopup(
-            'https://hannahlim1.typeform.com/to/LwxSXf',
-            {
-                mode: 'popup',
-                autoClose: 3000,
-                hideHeaders: true,
-                hideFooters: true,
-                onSubmit: function() {
-                    console.log('Typeform successfully submitted')
-                }
-            }
-        )
-        reference.open()
-    }
-    
 
     render(){
         return(
